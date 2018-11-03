9ba4ed6b8c5f3a862edc518b49ec050a064561fd
bfly <billfly@programmer.net>
2018-11-03 09:29:43 -0500

    Trying to fix User Registration, but getting a PRIMARY KEY ON PUBLIC.USERS(ID) error on the save operation.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO users(id,username,Encrypted_Password)
-VALUES (1,'steve','$2a$10$pC0F6knNw6NhpuWujg9XyeW0aZBcm.0co9Tcbhkv0SzjqtQur.a1G');
+VALUES (999,'steve','$2a$10$pC0F6knNw6NhpuWujg9XyeW0aZBcm.0co9Tcbhkv0SzjqtQur.a1G');
 INSERT INTO users(id,username,Encrypted_Password)
-VALUES (2,'bill','$2a$10$bRyIb0GbafP2rviKvkTDXukswVczuw.qG6bq8vEFCRFBbK6tkQVnq');
+VALUES (1000,'bill','$2a$10$bRyIb0GbafP2rviKvkTDXukswVczuw.qG6bq8vEFCRFBbK6tkQVnq');
 
-INSERT INTO roles (user_id, role) VALUES (1, 'ROLE_USER');
-INSERT INTO roles (user_id, role) VALUES (2, 'ROLE_ADMIN');
-INSERT INTO roles (user_id, role) VALUES (2, 'ROLE_USER');
+INSERT INTO roles (user_id, role) VALUES (999, 'ROLE_USER');
+INSERT INTO roles (user_id, role) VALUES (1000, 'ROLE_ADMIN');
+INSERT INTO roles (user_id, role) VALUES (1000, 'ROLE_USER');
